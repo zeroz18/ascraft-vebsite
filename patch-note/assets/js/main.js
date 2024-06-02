4e9246b965700ece9fc27be2c5cbc30e961f7473
zeroz18_off <127630632+zeroz18@users.noreply.github.com>
2024-06-02 20:17:10 +0200

    Update main.js

--- patch-note/assets/js/main.js
+++ patch-note/assets/js/main.js
@@ -33,7 +33,7 @@ function headerShadow() {
 
 /* ----- TYPING EFFECT ----- */
 var typingEffect = new Typed(".typedText",{
-  strings : ["Patch-Note","Bêta V1"],
+  strings : ["Patch-Note","Bêta V1.0.0"],
   loop : true,
   typeSpeed : 100, 
   backSpeed : 80,
